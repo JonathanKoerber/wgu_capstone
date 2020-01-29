c86868215a7763722819df1bfab2a88b8bc3f20f
Jonathan <jonathankoerber@gmail.com>
2020-01-28 16:05:08 -0800

    Reoragized the app into packages.

--- orange_it/__init__.py
+++ orange_it/__init__.py
@@ -1,36 +1,39 @@
 
-import os
-import dictionary_profile
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from orange_it.config import DevelopmentConfig
+from orange_it.config import DevelopmentConfig, Config
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
 
+
 # todo change db using environ var
-app = Flask(__name__)
-app.config.from_object(DevelopmentConfig)
-app.config['SECRET_KEY'] = '838a2fbb0c5f33c28d77e8f9c69932ba'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
+
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
 login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = dictionary_profile.email_profile.get('EMAIL_USER')
-app.config['MAIL_PASSWORD'] = dictionary_profile.email_profile.get('EMAIL_PASS')
+mail = Mail()
 
-mail = Mail(app)
 
-from orange_it.users.routes import users
-from orange_it.posts.routes import posts
-from orange_it.main.routes import main
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    app.config.from_object(DevelopmentConfig)
 
-app.register_blueprint(users)
-app.register_blueprint(posts)
-app.register_blueprint(main)
+    db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
 
+    from orange_it.users.routes import users
+    from orange_it.posts.routes import posts
+    from orange_it.main.routes import main
+    from orange_it.errors.handlers import errors
+    app.register_blueprint(users)
+    app.register_blueprint(posts)
+    app.register_blueprint(main)
+    app.register_blueprint(errors)
+
+    return app
--- run.py
+++ run.py
@@ -1,5 +1,6 @@
-from orange_it import app
+from orange_it import create_app
 
-
+app = create_app()
 if __name__ == '__main__':
     app.run(debug=True)
+
