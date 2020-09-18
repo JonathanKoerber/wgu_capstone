6cd975bde498911711755c064f86808c2714ef77
JonathanKoerber <jonathankoerber@gmail.com>
2020-09-17 20:57:48 -0700

    preping to deploy

--- ENV/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/jonathan/Documents/projects_python/orangeIT/ENV/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
--- orange_it/users/routes.py
+++ orange_it/users/routes.py
@@ -1,8 +1,7 @@
 from flask import Blueprint
 from flask import render_template, url_for, flash, redirect, request
 from flask_login import login_user, current_user, logout_user, login_required
-from orange_it.users.forms import (RegistrationForm, LoginForm, UpdateAccountForm,
-                                   RequestResetForm, ResetPasswordForm)
+from orange_it.users.forms import (RegistrationForm, LoginForm, UpdateAccountForm, RequestResetForm, ResetPasswordForm)
 from orange_it import db
 from orange_it.models import User, Post, Thread
 from orange_it.users.utils import save_picture, send_reset_email
