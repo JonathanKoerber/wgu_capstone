458415be7a05f657005e0401cd640f324af940be
Jonathan Koerber <jonathankoerber@gmail.com>
2021-01-21 09:06:21 -0800

    I did something

--- orange_it/main/routes.py
+++ orange_it/main/routes.py
@@ -11,17 +11,15 @@ main = Blueprint('main', __name__)
 def index():
     posts = Post.query.order_by(Post.date_posted.desc())
     threads = Thread.query.order_by(Thread.date_created.desc()).all()
-
     return render_template('index.html', posts=posts, threads=threads)
 
 @main.route('/search', methods=['GET', 'POST'])
 def search():
+    # todo error with search
     page = request.args.get('page', 1, type=int)
-
     num_posts = min(request.args.get('limit', 50), 50)
     search_str = request.args.get('query', '')
     posts = Post.query.search(search_str, num_posts)
-
     threads = Thread.query.order_by(Thread.date_created.desc()).all()
     return render_template('index.html', title=('Search'), posts=posts, threads=threads)
    # return render_template('index.html', posts=posts, threads=threads)
